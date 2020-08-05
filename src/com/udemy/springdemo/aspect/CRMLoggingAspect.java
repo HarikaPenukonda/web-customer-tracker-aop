253b2209da544426ef5c2e91f265daadda743b98
harika penukonda <penukondaharika@gmail.com>
2020-08-05 10:36:36 -0500

    Add @Before advice

--- src/com/udemy/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/udemy/springdemo/aspect/CRMLoggingAspect.java
@@ -2,7 +2,9 @@ package com.udemy.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -16,20 +18,33 @@ public class CRMLoggingAspect {
 	private Logger myLogger = Logger.getLogger(getClass().getName());
 	
 	// setup pointcut declarations for controller
-	@Pointcut("execution(* com.udemy.springdemo.controller.*.*.(..))")
+	@Pointcut("execution(* com.udemy.springdemo.controller.*.*(..))")
 	private void forControllerPackage() {}
 	
 	// setup pointcut declarations for service
-	@Pointcut("execution(* com.udemy.springdemo.service.*.*.(..))")
+	@Pointcut("execution(* com.udemy.springdemo.service.*.*(..))")
 	private void forServicePackage() {}
 	
 	// setup pointcut declarations for dao
-	@Pointcut("execution(* com.udemy.springdemo.dao.*.*.(..))")
+	@Pointcut("execution(* com.udemy.springdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
 	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()") // match on these three packages 
 	private void forAppFlow() {}
+	
 	// add @Before advice
+	@Before("forAppFLow")
+	public void before(JoinPoint theJoinPoint) {
+		
+		// display method we are calling
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====>> in @Before: calling method: " + theMethod);
+		
+		// display the arguments to the method
+		
+	}
+	
+	
 	
 	// add @AfterReturning advice
 
