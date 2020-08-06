6484af9c14a22f7b66626c1ebf778a605b1315d7
harika penukonda <penukondaharika@gmail.com>
2020-08-06 10:33:21 -0500

    Update CRMLoggingAspect.java

--- src/com/udemy/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/udemy/springdemo/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package com.udemy.springdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -42,10 +43,41 @@ public class CRMLoggingAspect {
 		
 		// display the arguments to the method
 		
+		// get the arguments
+		Object[] args = theJoinPoint.getArgs();
+		// loop thru and disply args
+		for(Object tempArg : args) {
+			myLogger.info("======>> argument: " + tempArg);
+		}
+	}
+	
+	// add @AfterReturning advice	
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		
+		// display method we are returning from
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====>> in @@AfterReturning: calling method: " + theMethod);
+		
+		// display data returned
+		myLogger.info("======>> result: " + theResult);
 	}
 	
 	
 	
-	// add @AfterReturning advice
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
